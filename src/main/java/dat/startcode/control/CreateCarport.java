83b9fb60cefe19cbab369bc477c7a0dc39af335a
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-05-09 14:14:19 +0200

    tilgføj beregning af spær

--- src/main/java/dat/startcode/control/CreateCarport.java
+++ src/main/java/dat/startcode/control/CreateCarport.java
@@ -1,5 +1,6 @@
 package dat.startcode.control;
 
+import dat.startcode.logic.RequestCalculator;
 import dat.startcode.model.exceptions.DatabaseException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 public class CreateCarport extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
+        RequestCalculator requestCalculator = new RequestCalculator();
+        requestCalculator.calculate();
+
         return "createCarport";
     }
 }
--- src/main/java/dat/startcode/logic/RequestCalculator.java
+++ src/main/java/dat/startcode/logic/RequestCalculator.java
@@ -10,49 +10,76 @@ public class RequestCalculator {
 
     CustomerRequest customerRequest = new CustomerRequest(600, 780, "flat");
 
+// Ganger med 10 for at få længderne i mm
+    int carpLengthInMm = customerRequest.getCarpLength() * 10;
+    int carpWidthInMm = customerRequest.getCarpWidth() * 10;
 
     /* 100 cm udhæng foran + maks afstand på 325 cm mellem stolper + 30 cm udhæng bagtil
      Derfor følgende breakpoint */
-    private final int COLUMNBREAKPOINT = 455;
-    private final int REMBREAKPOINT = 480;
+    private final int COLUMNBREAKPOINT = 4550;
+    private final int REMBREAKPOINT = 4800;
+    private final int MAXDISTANCERAFTERS = 600;
+    private final int RAFTERSTHICKNESS = 45;
+    private final int UNDERSTERNBRÆDDERTYKKELSE = 25;
 
-    public void calculate(CustomerRequest customerRequest) {
+
+    // public void calculate(CustomerRequest customerRequest) {
+    public void calculate() {
 
         List<MaterialDTO> listOfMaterial = new ArrayList<>();
 
         // Finder antal stolper på baggrund af længden
 
-        if (customerRequest.getCarpLength() <= COLUMNBREAKPOINT) {
+        if (carpLengthInMm <= COLUMNBREAKPOINT) {
 
             // Stolpe = materialID 4
 
-            listOfMaterial.add(new MaterialDTO(4,4));
+            listOfMaterial.add(new MaterialDTO(4, 4));
 
-        }
-        else {
+        } else {
             listOfMaterial.add(new MaterialDTO(4, 6));
         }
 
-        // Finder remme
+        // Finder remme //TODO: tager ikke højde for at der er remme i forskellige længder
 
-        if (customerRequest.getCarpLength() <= REMBREAKPOINT) {
+        if (carpLengthInMm <= REMBREAKPOINT) {
 
-            listOfMaterial.add(new MaterialDTO(5,2));
+            listOfMaterial.add(new MaterialDTO(5, 2));
 
+        } else {
+
+            listOfMaterial.add(new MaterialDTO(5, 4));
         }
-        else {
 
-            listOfMaterial.add(new MaterialDTO(5,4));
-        }
+        // Finder spær //TODO: tager pt ikke højde for at man kan få spær i forskellige længder
 
-        // Finder spær
+//        Ny længde = Længde på carport - tykkelse på spær - (tykkelse på understernbrædder * 2) (fordi det første spærs tykkelse ikke er medregnet når mellemrummene lægges sammen, og der er er understernbræt på foran og bagved)
+
+        int newLength = carpLengthInMm - RAFTERSTHICKNESS - (UNDERSTERNBRÆDDERTYKKELSE * 2);
+
+//        Antal mellemrum mellem spær = ny længde / max afstand mellem spær
+
+//        Antal mellemrum mellem spær = 7705 mm / 600 mm = 12,841
+        double spaceAmountBetweenRaftersDouble = (double) newLength / MAXDISTANCERAFTERS;
 
 
+//        Så skal man vel runde op, fordi det var en maxafstand?
+//
+//        Så det vil sige 13 mellemrum skal vi bruge og 14 spær
 
+        int spaceAmountBetweenRafters = (int) Math.ceil(spaceAmountBetweenRaftersDouble);
+
+        //Der skal altid være et spær mere end antallet af mellemrum mellem spær
+        int raftersAmount = spaceAmountBetweenRafters + 1;
+
+//        Afstand mellem spær = 7705 mm / 13 = 592,7 mm
+
+        double raftersDistance = (double) newLength / spaceAmountBetweenRafters;
+
+        //TODO: der skal også sendes en beskrivelse med af hvad træstykkerne skal bruges til
+        listOfMaterial.add(new MaterialDTO(54, raftersAmount));
 
     }
-
-
 
 
 }

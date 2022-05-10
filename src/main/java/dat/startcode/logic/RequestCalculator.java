4ace6f215a893d7ecac2c501f8bb72f71e015e02
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-05-10 13:27:25 +0200

    tilføj logik om tag

--- src/main/java/dat/startcode/logic/RequestCalculator.java
+++ src/main/java/dat/startcode/logic/RequestCalculator.java
@@ -11,17 +11,28 @@ public class RequestCalculator {
 
     CustomerRequest customerRequest = new CustomerRequest(600, 780, "flat");
 
-// Ganger med 10 for at få længderne i mm
+    // Ganger med 10 for at få længderne i mm
     int carpLengthInMm = customerRequest.getCarpLength() * 10;
     int carpWidthInMm = customerRequest.getCarpWidth() * 10;
 
     /* 100 cm udhæng foran + maks afstand på 325 cm mellem stolper + 30 cm udhæng bagtil
      Derfor følgende breakpoint */
+    //STOLPER
     private final int COLUMNBREAKPOINT = 4550;
+    //REMME
     private final int REMBREAKPOINT = 4800;
+    //SPÆR
     private final int MAXDISTANCERAFTERS = 600;
     private final int RAFTERSTHICKNESS = 45;
-    private final int UNDERSTERNBRÆDDERTYKKELSE = 25;
+    //UNDERSTERNBRÆDDER
+    private final int FASCIABOARDTHICKNESS = 25;
+    //TAG
+    //TODO: der er faktisk to breakpoints, da carportens længde kan være helt nede på 240 cm
+    private final int FLATROOFBIGBREAKPOINT = 5850;
+    private final int FLATROOFSMALLBREAKPOINT = 3450;
+    private final int ROOFPLATEWIDTH = 1090;
+    private final int ROOFPLATEOVERLAP = 110;
+
 
     //TODO: Hyggehejsa læser, dette skal ændres så det kommer fra et scope!
     private int orderId = 0;
@@ -34,7 +45,7 @@ public class RequestCalculator {
         List<MaterialDTO> listOfMaterial = new ArrayList<>();
         List<BillsOfMaterial> billsOfMaterials = new ArrayList<>();
 
-        // Finder antal stolper på baggrund af længden
+        // FINDER ANTAL STOLPER PÅ BAGGRUND AF LÆNGDEN
 
         if (carpLengthInMm <= COLUMNBREAKPOINT) {
             // Stolpe = materialID 4
@@ -44,7 +55,7 @@ public class RequestCalculator {
             billsOfMaterials.add(new BillsOfMaterial(bomId, 4, orderId, 6, "Stolper nedgraves 90 cm. i jord"));
         }
 
-        // Finder remme //TODO: tager ikke højde for at der er remme i forskellige længder
+        // FINDER REMME //TODO: tager ikke højde for at der er remme i forskellige længder
 
         if (carpLengthInMm <= REMBREAKPOINT) {
             billsOfMaterials.add(new BillsOfMaterial(bomId, 5, orderId, 2, "Remme i sider, sadles ned i stolper"));
@@ -53,11 +64,11 @@ public class RequestCalculator {
             billsOfMaterials.add(new BillsOfMaterial(bomId, 5, orderId, 4, "Remme i sider, sadles ned i stolper"));
         }
 
-        // Finder spær //TODO: tager pt ikke højde for at man kan få spær i forskellige længder
+        // FINDER SPÆR //TODO: tager pt ikke højde for at man kan få spær i forskellige længder
 
 //        Ny længde = Længde på carport - tykkelse på spær - (tykkelse på understernbrædder * 2) (fordi det første spærs tykkelse ikke er medregnet når mellemrummene lægges sammen, og der er er understernbræt på foran og bagved)
 
-        int newLength = carpLengthInMm - RAFTERSTHICKNESS - (UNDERSTERNBRÆDDERTYKKELSE * 2);
+        int newLength = carpLengthInMm - RAFTERSTHICKNESS - (FASCIABOARDTHICKNESS * 2);
 
 //        Antal mellemrum mellem spær = ny længde / max afstand mellem spær
 
@@ -66,7 +77,6 @@ public class RequestCalculator {
 
 
 //        Så skal man vel runde op, fordi det var en maxafstand?
-//
 //        Så det vil sige 13 mellemrum skal vi bruge og 14 spær
 
         int spaceAmountBetweenRafters = (int) Math.ceil(spaceAmountBetweenRaftersDouble);
@@ -80,8 +90,45 @@ public class RequestCalculator {
 
         billsOfMaterials.add(new BillsOfMaterial(bomId, 54, orderId, raftersAmount, "Spær, monteres på rem"));
 
-        System.out.println();
         //TODO: Kald mapper der tager en List<BillsOfMaterials> som parameter, looper over dem og stempler dem ned i databasen.
+
+        //BEREGNING AF TAGPLADER
+
+        //bølgerne på tagpladerne går på langs med pladernes længde. Og bølgerne skal følge tagets hældning, så vandet kan løbe af skuret.
+
+        // taget skal gå 5 cm ud over sternbredderne på alle leder, derfor lægges 10 cm til
+        int roofLength = carpLengthInMm + 100;
+        int roofWidth = carpWidthInMm + 100;
+
+        int roofPlateWidthMinusOverlap = ROOFPLATEWIDTH - ROOFPLATEOVERLAP;
+
+        // overlappet mellem pladerne på den korte led er 11 cm, da godt 1,5 bølger skal overlappe og der er 7 cm fra bølgetop til bølgetop
+        // den sidste plade har sin fulde bredde
+        // overlappet på den lange led skal være 20 cm
+
+        double plateAmountDouble = ((double) roofWidth - ROOFPLATEOVERLAP) / roofPlateWidthMinusOverlap;
+        //TODO her kunne være en if der chekkede om decimaltallet er meeeget lille og det giver bedre mening at runde ned
+        int plateAmount = (int) Math.ceil(plateAmountDouble);
+        int shortRoofPlatesAmount = 0;
+
+        if(roofLength > FLATROOFSMALLBREAKPOINT) {
+            shortRoofPlatesAmount = plateAmount;
+            billsOfMaterials.add(new BillsOfMaterial(bomId, 29, orderId, shortRoofPlatesAmount, "tagplader monteres på spær"));
+        }
+        else {
+
+            if (roofLength > FLATROOFBIGBREAKPOINT) {
+                shortRoofPlatesAmount = plateAmount;
+                billsOfMaterials.add(new BillsOfMaterial(bomId, 29, orderId, shortRoofPlatesAmount, "tagplader monteres på spær"));
+            }
+
+            int longRoofPlatesAmount = plateAmount;
+            billsOfMaterials.add(new BillsOfMaterial(bomId, 28, orderId, longRoofPlatesAmount, "tagplader monteres på spær"));
+        }
+        System.out.println();
+
+        //DELE DER ALTID SKAL MED
+
 
     }
 

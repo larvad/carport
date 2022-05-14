88d391c5403824c758c3e942daa4006bc84443cb
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-05-14 18:25:27 +0200

    laver foreløbig order og BOM, det virker

--- src/main/java/dat/startcode/control/SendInquiry.java
+++ src/main/java/dat/startcode/control/SendInquiry.java
@@ -1,5 +1,6 @@
 package dat.startcode.control;
 
+import dat.startcode.logic.RequestCalculator;
 import dat.startcode.model.config.ApplicationStart;
 import dat.startcode.model.entities.Inquiry;
 import dat.startcode.model.entities.Order;
@@ -63,11 +64,9 @@ public class SendInquiry extends Command {
         int inquiryID = inquiry.getInquiryId();
         Order earlyOrder = UserFacade.insertEarlyOrderIntoDB(userId, inquiryID, connectionPool);
 
-        //Få orderIDet ind i RequestCalculator
-
-        // kald beregning, som laver BOM
-
-        //Lav stykliste
+        //Få orderIDet ind i RequestCalculator + kald beregning, som laver BOM
+        RequestCalculator requestCalculator = new RequestCalculator();
+        requestCalculator.calculate(earlyOrder.getOrderId(), connectionPool);
 
         return "confirmInquiry";        //TODO: nice to have: sætte nogle krav til skur mål
     }

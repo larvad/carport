708b0db59df04cd1031d789500e0233c1645f908
Ot2 <Otto.PKristensen@gmail.com>
2022-05-17 14:13:31 +0200

    godkend virker, med ny klasse

--- src/main/java/dat/startcode/control/AdminApprove.java
+++ src/main/java/dat/startcode/control/AdminApprove.java
@@ -1,13 +1,33 @@
 package dat.startcode.control;
 
+import dat.startcode.model.config.ApplicationStart;
 import dat.startcode.model.exceptions.DatabaseException;
+import dat.startcode.model.persistence.ConnectionPool;
+import dat.startcode.model.services.UserFacade;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AdminApprove extends Command{
+    private ConnectionPool connectionPool;
+
+    public AdminApprove() {
+        this.connectionPool = ApplicationStart.getConnectionPool();
+    }
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
-        return "adminApprove";
+
+        boolean order;
+
+        int orderId = Integer.parseInt(request.getParameter("approve"));
+
+        String ChangeStatusID = request.getParameter("changeStatus");
+
+        order = UserFacade.setOrderStatusByOrderId(orderId, connectionPool);
+
+        Admin admin = new Admin();
+
+        return admin.execute(request,response);
+
     }
 }

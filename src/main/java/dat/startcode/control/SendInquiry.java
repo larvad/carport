a7f9745136f7d7601f7a016663d48eae101918e8
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-05-18 10:56:40 +0200

    finpudsning

--- src/main/java/dat/startcode/control/SendInquiry.java
+++ src/main/java/dat/startcode/control/SendInquiry.java
@@ -73,7 +73,7 @@ public class SendInquiry extends Command {
         //Generér OderId
         int orderId = UserFacade.insertOrderIntoDB(inquiry.getInquiryId(), user.getUserId(), 1, connectionPool);
 
-        session.setAttribute("inquiry", inquiry);
+        request.setAttribute("inquiry", inquiry);
 
         int userId = user.getUserId();
         int inquiryID = inquiry.getInquiryId();

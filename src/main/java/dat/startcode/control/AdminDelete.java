c615570299a6b1c3c6223a51e1d31e0237099c25
Timea Ballok <cph-tb245@cphbusiness.dk>
2022-05-18 09:51:09 +0200

    lav slet funktion

--- src/main/java/dat/startcode/control/AdminDelete.java
+++ src/main/java/dat/startcode/control/AdminDelete.java
@@ -1,13 +1,30 @@
 package dat.startcode.control;
 
+import dat.startcode.model.config.ApplicationStart;
 import dat.startcode.model.exceptions.DatabaseException;
+import dat.startcode.model.persistence.ConnectionPool;
+import dat.startcode.model.services.UserFacade;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
 
 public class AdminDelete extends Command{
+    private ConnectionPool connectionPool;
+
+    public AdminDelete() {
+        this.connectionPool = ApplicationStart.getConnectionPool();
+    }
+
+
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
-        return "adminDelete";
+        boolean succes;
+
+        int orderId = Integer.parseInt(request.getParameter("delete"));
+        succes = UserFacade.deleteOrderByOrderId(orderId, connectionPool);
+
+        Admin admin = new Admin();
+        return admin.execute(request,response);
     }
 }

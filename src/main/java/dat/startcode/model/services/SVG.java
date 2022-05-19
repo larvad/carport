4f7e9bdf6aba3df26e9b80f6889192d2732a66ac
Timea Ballok <cph-tb245@cphbusiness.dk>
2022-05-19 09:02:27 +0200

    tegn carport efter mål uden skur

--- src/main/java/dat/startcode/model/services/SVG.java
+++ src/main/java/dat/startcode/model/services/SVG.java
@@ -9,15 +9,15 @@ public class SVG {
     private int width;
     private int height;
 
-    private final String headerTemplate = "<svg height=\"%d%%\" " +
-            "width=\"%d%%\" " +
+    private final String headerTemplate = "<svg height=\"%d\" " +
+            "width=\"%d\" " +
             "viewBox=\"%s\" "+
             "x=\"%d\"   " +
             "y=\"%d\"   " +
             " preserveAspectRatio=\"xMinYMin\">";
 
-    private final String rectTemplate = "<rect x=\"%d\" y=\"%d\" height=\"%f\" width=\"%f\" style=\"stroke:#000000; fill: #ffffff\" />";
-    private final String lineTemplate = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\" stroke-dasharray=\"4\" />";
+    private final String rectTemplate = "<rect x=\"%f\" y=\"%f\" height=\"%f\" width=\"%f\" style=\"stroke:#000000; fill: #ffffff\" />";
+    private final String lineTemplate = "<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" stroke=\"black\" stroke-dasharray=\"4\" />";
 
     public SVG (int x, int y, String viewBox, int width, int height) {
         this.x = x;
@@ -28,7 +28,7 @@ public class SVG {
         svg.append(String.format(headerTemplate, height, width, viewBox, x, y ));
     }
 
-    public void addRect(int x, int y, double height, double width) {
+    public void addRect(double x, double y, double height, double width) {
         svg.append(String.format(rectTemplate, x, y, height, width));
     }
 
@@ -36,7 +36,7 @@ public class SVG {
 //
 //    }
 
-    public void addStripedLine(int x1, int y1, int x2, int y2){
+    public void addStripedLine(double x1, double y1, double x2, double y2){
         svg.append(String.format(lineTemplate, x1, y1, x2, y2));
     }
 

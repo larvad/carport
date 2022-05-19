86e8c33d67dbec90d35bc22dbfc7650da66cf1c3
Nicki Kielsen <cph-nk11@cphbusiness.dk>
2022-05-19 13:21:46 +0200

    til timi

--- src/main/java/dat/startcode/control/ShowSVG.java
+++ src/main/java/dat/startcode/control/ShowSVG.java
@@ -33,8 +33,8 @@ public class ShowSVG extends Command{
         double remThickness = 4.5;
         double columnThickness = 9.7;
 
-        SVG svg2 = new SVG(100, 100, "0 0 "+ carpWidth + " " + carpLength, 800, 640 );
         SVG svg1 = new SVG(0, 0, "0 0 "+ carpWidth + " " + carpLength, 1000, 1000 );
+        SVG svg2 = new SVG(100, 100, "0 0 "+ carpWidth + " " + carpLength, 800, 640 );
 
         drawCarportRoof(svg2, carpWidth, carpLength);
         drawShed(svg2, carpWidth, carpLength, shedWidth, shedLength);
@@ -42,7 +42,8 @@ public class ShowSVG extends Command{
         drawRems(svg2, carpWidth, carpLength, rafterThickness);
         drawCross(svg2, carpWidth, carpLength, remThickness);
         drawColumnWithoutShed(svg2, carpWidth, carpLength, columnThickness);
-        svg1.addSvg(drawArrows(svg2, carpWidth, carpLength));
+        drawArrows(svg2, carpWidth, carpLength);
+        svg1.addSvg(svg2);
 
         request.setAttribute("svg", svg1.toString());
         return "svg";
@@ -115,10 +116,9 @@ public class ShowSVG extends Command{
     }
 
     private SVG drawArrows(SVG svg, int carpWidth, int carpLength) {
-        SVG svg3 = new SVG(0, 0, "0 0 "+ carpWidth + " " + carpLength, 1000, 1000 );
-        svg3.addRect(0,0,1000, 1000);
-        svg3.addArrows(0,0, 0, carpWidth);
-        return svg3;
+        svg.addRect(0,0,1000, 1000);
+        svg.addArrows(0,0, 0, carpWidth);
+        return svg;
     }
 
 
